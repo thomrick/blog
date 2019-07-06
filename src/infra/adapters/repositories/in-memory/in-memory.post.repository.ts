fb3bf1308bc7c1740cf9b2cc73ec2034dcb2f1bc
Thomas Ricart <thomasr2309@gmail.com>
2019-07-06 23:09:08 +0200

    feature(post) : implement get all post respository method

--- src/infra/adapters/repositories/in-memory/in-memory.post.repository.ts
+++ src/infra/adapters/repositories/in-memory/in-memory.post.repository.ts
@@ -1,23 +1,32 @@
 import { PostAggregate, PostRepository } from '../../../../domain';
+import { PostEvent } from '../../../../domain/events';
 
 export class InMemoryPostRepository implements PostRepository {
 
-  private database: Map<string, PostAggregate>;
+  private database: Map<string, PostEvent[]>;
 
-  constructor(database: Map<string, PostAggregate> = new Map()) {
+  constructor(database: Map<string, PostEvent[]> = new Map()) {
     this.database = database;
   }
 
   public save(post: PostAggregate): void {
-    this.database.set(post.getId(), post);
+    this.database.set(post.getId(), post.getUncommittedChanges());
   }
 
   public get(id: string): PostAggregate | null {
-    const aggregate: PostAggregate | undefined = this.database.get(id);
-    return !!aggregate ? aggregate : null;
+    const events: PostEvent[] | undefined = this.database.get(id);
+    return !!events ? PostAggregate.rebuild(events) : null;
   }
 
   public getAll(): PostAggregate[] {
-    return [];
+    const iterator: Iterator<PostEvent[]> = this.database.values();
+    const posts: PostAggregate[] = [];
+    let result: IteratorResult<PostEvent[]> = iterator.next();
+    while (!result.done) {
+      const events: PostEvent[] = result.value;
+      posts.push(PostAggregate.rebuild(events));
+      result = iterator.next();
+    }
+    return posts;
   }
 }
